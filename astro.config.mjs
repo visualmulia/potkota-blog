07ffb1c458f5b47689df22781219eb45e5f9a6ca
visualmulia <visualmulia@gmail.com>
2026-07-15 16:37:37 +0800

    Update canonical site URL to potkota.web.id and add sitemap.xml rewrite route

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.mesinwaktu.web.id',
+  site: 'https://www.potkota.web.id',
   integrations: [sitemap()]
 });
